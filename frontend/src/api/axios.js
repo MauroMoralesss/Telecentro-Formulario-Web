9cca3b62d1ae905c9a81f379392d56a04c3f60ee
MauroMoraless <moralesmaurot6@gmail.com>
2025-04-07 02:00:10 -0300

    preparado para deploy

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseURL = process.env.VITE_BACKEND || "http://localhost:3000/api";
+
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL,
   withCredentials: true
 });
 
--- src/config.js
+++ src/config.js
@@ -6,4 +6,4 @@ export const PG_USER = process.env.PG_USER || "postgres";
 export const PG_PASSWORD = process.env.PG_PASSWORD || "root";
 export const PG_DATABASE = process.env.PG_DATABASE || "formv1";
 
-export const ORIGIN = process.env.ORIGIN || "http://localhost:5173";
+export const ORIGIN = process.env.ORIGIN || "http://192.168.0.148:4000";
