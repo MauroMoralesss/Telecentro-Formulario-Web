fed1bbec7bf8debef724fc3ef7bc4d378b073fcc
MauroMoraless <moralesmaurot6@gmail.com>
2025-04-07 03:15:20 -0300

    cambiando origin

--- src/config.js
+++ src/config.js
@@ -6,4 +6,5 @@ export const PG_USER = process.env.PG_USER || "postgres";
 export const PG_PASSWORD = process.env.PG_PASSWORD || "root";
 export const PG_DATABASE = process.env.PG_DATABASE || "formv1";
 
-export const ORIGIN = process.env.ORIGIN || "http://192.168.0.148:4000";
+export const ORIGIN = process.env.ORIGIN;
+
