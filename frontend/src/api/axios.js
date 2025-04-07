732c7f39c8b167933c878064a56d8466d0daac5b
MauroMoraless <moralesmaurot6@gmail.com>
2025-04-07 04:26:40 -0300

    nuevo build para netlify

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.VITE_BACKEND || "http://localhost:3000/api";
+const baseURL = import.meta.env.VITE_BACKEND || "http://localhost:3000/api";
 
 const instance = axios.create({
   baseURL,
