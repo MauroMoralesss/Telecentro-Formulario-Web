34220f681738f99172f0a2942dfa495fe79f357b
MauroMoraless <moralesmaurot6@gmail.com>
2025-04-14 17:05:18 -0300

    Agrega configuración SSL para conexión a PostgreSQL

--- src/db.js
+++ src/db.js
@@ -1,20 +1,23 @@
 import {
-    PG_DATABASE,
-    PG_HOST,
-    PG_PASSWORD,
-    PG_PORT,
-    PG_USER,
-  } from "./config.js";
-  import pg from "pg";
-  
-  export const pool = new pg.Pool({
-    port: PG_PORT,
-    host: PG_HOST,
-    user: PG_USER,
-    password: PG_PASSWORD,
-    database: PG_DATABASE,
-  });
-  
-  pool.on("connect", () => {
-    console.log("Database connected");
-  });
+  PG_DATABASE,
+  PG_HOST,
+  PG_PASSWORD,
+  PG_PORT,
+  PG_USER,
+} from "./config.js";
+import pg from "pg";
+
+export const pool = new pg.Pool({
+  port: PG_PORT,
+  host: PG_HOST,
+  user: PG_USER,
+  password: PG_PASSWORD,
+  database: PG_DATABASE,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+pool.on("connect", () => {
+  console.log("Database connected");
+});
